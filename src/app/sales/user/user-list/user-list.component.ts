6c115b1195e604553151956d7764b78384bd9a22
Nicholas Preshaw <nickpreshaw@yahoo.com>
2022-04-06 09:04:29 -0400

    initial commit

---
+++ src/app/sales/user/user-list/user-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user.class';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  users: User[] = []
+
+  constructor(
+    private usersvc: UserService
+  ) { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
